ce02dd5ffab9a4bf570c40a5e90aac05da597e8f
junaid-southville <junaid.ahmad@southvillesol.com>
2023-12-11 18:15:21 +0500

    Initial commit

---
+++ components/LoadMore.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import Image from "next/image";
+import {useInView} from "react-intersection-observer";
+import {useEffect, useState} from "react";
+import {fetchAnime} from "@/app/actions/actions";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element
+function LoadMore() {
+    const [ref, inView] = useInView();
+    const [loadData, setLoadData] = useState<AnimeCard[]>([]);
+
+    useEffect(() => {
+        if (inView) {
+            fetchAnime(page).then((response) => {
+                setLoadData([...loadData, ...response])
+                page++;
+            })
+        }
+    }, [inView, loadData])
+    return (
+        <>
+            <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+                {loadData}
+            </section>
+            <section className="flex justify-center items-center w-full">
+                <div ref={ref}>
+                    <Image
+                        src="./spinner.svg"
+                        alt="spinner"
+                        width={56}
+                        height={56}
+                        className="object-contain"
+                    />
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default LoadMore;
